2a3e5972d29cdad81403d681c7a396ffa749853e
alex-belomor <43720950+alex-belomor@users.noreply.github.com>
2018-11-15 20:51:58 +0200

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,1 +1,22 @@
+var MongoClient = require('mongodb').MongoClient;
 
+var db;
+
+exports.connect = function (url, done) {
+    if (db) {
+        return done();
+    }
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            return done(err);
+        }
+
+        db = db.db('local');
+        done();
+    });
+}
+
+exports.get = function () {
+    return db;
+}
